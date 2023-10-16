ca96c8dab182a508c0a0776b63fc25a5e14e65ba
gabrieltsants <146135815+gabrieltsants@users.noreply.github.com>
2023-10-16 00:30:46 -0300

    config update for CLI

--- configure.sh
+++ configure.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd $(dirname "$0");
-
 cd PHPLinkTester && composer install && composer dumpautoload;
+cd ../PHPLinkTesterCLI && composer install && composer dumpautoload;
 cd ../PHPLinkTesterWeb && composer install && composer dumpautoload;
 
 echo "Script finished";
